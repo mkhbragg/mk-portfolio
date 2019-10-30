6baf4de9595a96957849cb0d483157ec4b57879f
mkhbragg <mkhbragg@gmail.com>
2019-10-30 08:58:46 -0400

    Spicing up Experience page with rainbow css.

--- src/components/ExperienceItem/ExperienceItem.tsx
+++ src/components/ExperienceItem/ExperienceItem.tsx
@@ -4,11 +4,13 @@ import './ExperienceItem.scss';
 
 const experienceItem = (props: any) => {
     return (
-        <div className="ExperienceItem">
-            <div className="Heading">{ props.experience.company }</div>
-            <div className="Dates">{ props.experience.startDate } - { props.experience.endDate }</div>
-            <div className="Role">{ props.experience.role }</div>
-            <div className="Description">{ props.experience.description }</div>
+        <div className="ExperienceItemContainer">
+            <div className="ExperienceItem">
+                <div className="Heading">{ props.experience.company }</div>
+                <div className="Dates">{ props.experience.startDate } - { props.experience.endDate }</div>
+                <div className="Role">{ props.experience.role }</div>
+                <div className="Description">{ props.experience.description }</div>
+            </div>
         </div>
     );
 }

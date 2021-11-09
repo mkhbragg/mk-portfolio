beeadbedf50eef7928ed40b9dd62cab92d10713c
Mariah Bragg <mariah.bragg@block.one>
2021-11-09 16:12:45 -0500

    Style tweaks + some more traits

--- src/containers/About/About.tsx
+++ src/containers/About/About.tsx
@@ -1,24 +1,30 @@
-import React from 'react';
+import React from "react";
 
-import ScrollingText from '../../components/UI/ScrollingText/ScrollingText';
-import styles from './About.module.scss';
-import photo from '../../assets/images/mkhb_grayscale.jpg';
+import ScrollingText from "../../components/UI/ScrollingText/ScrollingText";
+import styles from "./About.module.scss";
+import photo from "../../assets/images/mkhb_grayscale.jpg";
 
 const about = () => {
-    const abilities = [
-        { id: 0, label: 'full-stack developer' },
-        { id: 1, label: 'researcher' },
-        { id: 2, label: 'UX hobbyist' },
-        { id: 3, label: 'tinkerer' },
-        { id: 4, label: 'curious mind' }
-    ];
+  const abilities = [
+    { id: 0, label: "UI developer" },
+    { id: 1, label: "researcher" },
+    { id: 2, label: "UX hobbyist" },
+    { id: 3, label: "tinkerer" },
+    { id: 4, label: "curious mind" },
+    { id: 5, label: "jiu jitsu purple belt" },
+    { id: 5, label: "Lego addict" },
+  ];
 
-    return (
-        <div className={styles.About}>
-            <div>Hi! I'm Mariah, a highly motivated <ScrollingText items={abilities} /> in pursuit of best practices, intuitive designs, and compelling groups of three.</div>
-            <img src={photo} alt="Mariah" />
-        </div>
-    );
-}
+  return (
+    <div className={styles.About}>
+      <div>
+        Hi! I'm Mariah, a highly motivated <ScrollingText items={abilities} />{" "}
+        in pursuit of best practices, intuitive designs, and compelling groups
+        of three.
+      </div>
+      <img src={photo} alt="Mariah" style={{ borderRadius: "50px" }} />
+    </div>
+  );
+};
 
 export default about;

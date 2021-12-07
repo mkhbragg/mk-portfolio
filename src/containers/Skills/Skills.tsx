77dd9d24247e4a6309fbaec37be3171ec15a85c4
Mariah Bragg <mk.bragg@pm.me>
2021-12-06 21:55:58 -0500

    Use bar charts with refreshed colors instead of pie charts. Updated skill data logic

--- src/containers/Skills/Skills.tsx
+++ src/containers/Skills/Skills.tsx
@@ -1,48 +1,105 @@
-import React, { Component } from 'react';
-import { Route, withRouter, Switch, Redirect, NavLink } from 'react-router-dom';
+import React, { Component } from "react";
+import { Route, withRouter, Switch, Redirect, NavLink } from "react-router-dom";
 
-import PieChart from '../../components/UI/Charts/PieChart/PieChart';
-import { SKILL_DATA, LIBSKILL_DATA, GENSKILL_DATA } from '../../resources/SkillData';
-import styles from '../../assets/sass/style.module.scss';
-import './Skills.scss';
+import BarChart from "../../components/UI/Charts/BarChart/BarChart";
+import { SKILL_DATA, LIBSKILL_DATA } from "../../resources/SkillData";
+import styles from "../../assets/sass/style.module.scss";
+import "./Skills.scss";
 
 class Skills extends Component<any, any> {
+  state = {
+    selectedChart: "core",
+  };
 
-    state = {
-        selectedChart: 'core'
-    }
+  render() {
+    const fontColor =
+      this.props.themeName === "dark-theme"
+        ? styles.darkThemeDefaultText
+        : styles.lightThemeDefaultText;
 
-    render() {
-        const options = { 
-            legend: {
-                labels: {
-                    fontColor: this.props.themeName === 'dark-theme' ? styles.darkThemeDefaultText : styles.lightThemeDefaultText,
-                    fontFamily: 'Futura',
-                    boxWidth: 10
-                },
-                position: 'bottom'
-            }
-        };
+    const secondaryFontColor =
+      this.props.themeName === "dark-theme"
+        ? styles.darkThemeSecondaryText
+        : styles.lightThemeSecondaryText;
 
-        return (
-            <div className="Skills">
-                <nav>
-                    <ul>
-                        <li><NavLink to={`${this.props.match.url}/core`}>Core</NavLink></li>
-                        <li><NavLink to={`${this.props.match.url}/tools`}>Tools &amp; Libraries</NavLink></li>
-                        <li><NavLink to={`${this.props.match.url}/general`}>General</NavLink></li>
-                    </ul>
-                </nav>
-                <p>Each segment of the chart below expresses years of experience in a given area.</p>
-                <Switch>
-                    <Route path={`${this.props.match.url}/core`} exact component={() => <PieChart data={SKILL_DATA} options={options} /> } />
-                    <Route path={`${this.props.match.url}/tools`} exact component={() => <PieChart data={LIBSKILL_DATA} options={options} /> } />
-                    <Route path={`${this.props.match.url}/general`} exact component={() => <PieChart data={GENSKILL_DATA} options={options} /> } />
-                    <Redirect from="/" to={`${this.props.match.url}/core`} />
-                </Switch>
-            </div>
-        );
-    }
+    const options = {
+      scales: {
+        xAxis: {
+          grid: {
+            color: secondaryFontColor,
+          },
+          ticks: {
+            color: fontColor,
+          },
+        },
+        yAxis: {
+          grid: {
+            color: secondaryFontColor,
+          },
+          ticks: {
+            beginAtZero: true,
+            color: fontColor,
+          },
+        },
+      },
+      plugins: {
+        legend: {
+          labels: {
+            color: fontColor,
+            font: "Futura",
+            boxWidth: 30,
+          },
+          position: "bottom",
+        },
+      },
+    };
+
+    return (
+      <div className="Skills">
+        <nav>
+          <ul>
+            <li>
+              <NavLink to={`${this.props.match.url}/core`}>Core</NavLink>
+            </li>
+            <li>
+              <NavLink to={`${this.props.match.url}/tools`}>
+                Tools &amp; Libraries
+              </NavLink>
+            </li>
+            {/* <li>
+              <NavLink to={`${this.props.match.url}/general`}>General</NavLink>
+            </li> */}
+          </ul>
+        </nav>
+        <p>
+          Each segment of the chart below expresses years of experience in a
+          given area.
+        </p>
+        <Switch>
+          <Route
+            path={`${this.props.match.url}/core`}
+            exact
+            component={() => <BarChart data={SKILL_DATA} options={options} />}
+          />
+          <Route
+            path={`${this.props.match.url}/tools`}
+            exact
+            component={() => (
+              <BarChart data={LIBSKILL_DATA} options={options} />
+            )}
+          />
+          {/* <Route
+            path={`${this.props.match.url}/general`}
+            exact
+            component={() => (
+              <BarChart data={GENSKILL_DATA} options={options} />
+            )}
+          /> */}
+          <Redirect from="/" to={`${this.props.match.url}/core`} />
+        </Switch>
+      </div>
+    );
+  }
 }
 
 export default withRouter(Skills);
--- src/resources/SkillData.tsx
+++ src/resources/SkillData.tsx
@@ -1,66 +1,168 @@
-export const SKILL_DATA = {
-    datasets: [
-        {
-            data: [6, 3, 5, 5],
-            backgroundColor: [
-                'rgba(1, 115, 120)',
-                'rgba(37, 155, 161)',
-                'rgba(13, 47, 133)',
-                'rgba(54, 89, 177)'
-            ],
-            borderColor: 'rgba(0,0,0,0)'
-        }
-    ],
-    labels: [
-        'JavaScript',
-        'HTML5/CSS3',
-        'TypeScript',
-        'Node.js'
-    ]
+import moment from "moment";
+
+interface Skill {
+  years: number;
+  label: string;
 }
+
+const BORDER_WIDTH = 1;
+const BORDER_RADIUS = 3;
+
+const WARM_COLORS = [
+  "249, 65, 68",
+  "243, 114, 44",
+  "248, 150, 30",
+  "249, 199, 79",
+];
+
+const COOL_COLORS = ["151, 223, 252", "142, 181, 240"];
+
+const PINK_COLORS = [
+  "234, 105, 139",
+  "213, 93, 146",
+  "192, 82, 153",
+  "172, 70, 161",
+  "151, 58, 168",
+  "130, 47, 175",
+  "109, 35, 182",
+  "100, 17, 173",
+  "87, 16, 137",
+];
+
+const CORE_SKILLS = [
+  {
+    label: "JavaScript",
+    years: moment().diff(moment("201401", "YYYYMM"), "years"),
+  },
+  {
+    label: "HTML5/CSS3",
+    years: moment().diff(moment("201401", "YYYYMM"), "years"),
+  },
+  {
+    label: "TypeScript",
+    years: moment().diff(moment("201810", "YYYYMM"), "years"),
+  },
+  {
+    label: "Node.js",
+    years: moment("201905", "YYYYMM").diff(moment("201403", "YYYYMM"), "years"),
+  },
+];
+
+const GENERAL_SKILLS = [
+  {
+    label: "Object-Oriented Programming",
+    years: 9,
+  },
+  {
+    label: "RESTful Development",
+    years: 6,
+  },
+];
+
+const TOOL_SKILLS = [
+  {
+    label: "Angular",
+    years: 4,
+  },
+  {
+    label: "React",
+    years: 3,
+  },
+  {
+    label: "jQuery",
+    years: 2,
+  },
+  {
+    label: "Sass",
+    years: 4,
+  },
+  {
+    label: "LESS",
+    years: 3,
+  },
+  {
+    label: "Git",
+    years: 7,
+  },
+  {
+    label: "Jest",
+    years: 0.5,
+  },
+  {
+    label: "Gulp/Grunt",
+    years: 2,
+  },
+  {
+    label: "Emotion",
+    years: 1.5,
+  },
+];
+
+const sortedSkills = (set: Skill[]) => set.sort((a, b) => a.years - b.years);
+
+const applyOpacityToColor = (color: string, opacity: number): string => {
+  return `rgba(${color}, ${opacity})`;
+};
+
+export const SKILL_DATA = {
+  datasets: [
+    {
+      label: "Core Skills",
+      data: sortedSkills(CORE_SKILLS).map((skill) => skill.years),
+      backgroundColor: WARM_COLORS.map((color: string) =>
+        applyOpacityToColor(color, 0.6)
+      ),
+      borderColor: WARM_COLORS.map((color: string) =>
+        applyOpacityToColor(color, 1)
+      ),
+      borderWidth: BORDER_WIDTH,
+      borderRadius: BORDER_RADIUS,
+      hoverBackgroundColor: WARM_COLORS.map((color: string) =>
+        applyOpacityToColor(color, 1)
+      ),
+    },
+  ],
+  labels: sortedSkills(CORE_SKILLS).map((skill) => skill.label),
+};
 
 export const GENSKILL_DATA = {
-    datasets: [
-        {
-            data: [7, 6],
-            backgroundColor: [
-                'rgba(255, 171, 0)',
-                'rgba(255, 222, 58)'
-            ],
-            borderColor: 'rgba(0,0,0,0)'
-        }
-    ],
-    labels: [
-        'Object-Oriented Programming',
-        'RESTful Development'
-    ]
-}
+  datasets: [
+    {
+      label: "General Skills",
+      data: GENERAL_SKILLS.map((skill) => skill.years),
+      backgroundColor: COOL_COLORS.map((color: string) =>
+        applyOpacityToColor(color, 0.6)
+      ),
+      borderColor: COOL_COLORS.map((color: string) =>
+        applyOpacityToColor(color, 1)
+      ),
+      borderWidth: BORDER_WIDTH,
+      borderRadius: BORDER_RADIUS,
+      hoverBackgroundColor: COOL_COLORS.map((color: string) =>
+        applyOpacityToColor(color, 1)
+      ),
+    },
+  ],
+  labels: GENERAL_SKILLS.map((skill) => skill.label),
+};
 
 export const LIBSKILL_DATA = {
-    datasets: [
-        {
-            data: [4, 1, 2, 4, 3, 5, 0.5, 2],
-            backgroundColor: [
-                'rgba(160, 0, 87)',
-                'rgba(209, 47, 135)',
-                'rgba(219, 86, 159)',
-                'rgba(154, 81, 192)',
-                'rgba(116, 9, 170)',
-                'rgba(132, 47, 176)',
-                'rgba(89, 6, 132)',
-                'rgba(70, 4, 104)'
-            ],
-            borderColor: 'rgba(0,0,0,0)'
-        }
-    ],
-    labels: [
-        'Angular',
-        'React',
-        'jQuery',
-        'Sass',
-        'LESS',
-        'Git',
-        'Jest',
-        'Gulp/Grunt'
-    ]
-}
+  datasets: [
+    {
+      label: "Tools & Libraries",
+      data: sortedSkills(TOOL_SKILLS).map((skill) => skill.years),
+      backgroundColor: PINK_COLORS.map((color: string) =>
+        applyOpacityToColor(color, 0.6)
+      ),
+      borderColor: PINK_COLORS.map((color: string) =>
+        applyOpacityToColor(color, 1)
+      ),
+      borderWidth: BORDER_WIDTH,
+      borderRadius: BORDER_RADIUS,
+      hoverBackgroundColor: PINK_COLORS.map((color: string) =>
+        applyOpacityToColor(color, 1)
+      ),
+    },
+  ],
+  labels: sortedSkills(TOOL_SKILLS).map((skill) => skill.label),
+};

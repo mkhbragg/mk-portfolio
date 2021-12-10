5e995d5c066e09d6a992ed68f52f2f08737387b6
Mariah Bragg <mk.bragg@pm.me>
2021-12-10 13:44:37 -0500

    Add animation to Portfolio. Use cards for experience items

--- src/App.tsx
+++ src/App.tsx
@@ -1,27 +1,26 @@
-import React, { useState } from 'react';
-import { BrowserRouter, Switch, Route } from 'react-router-dom';
+import React, { useState } from "react";
+import { BrowserRouter, Switch, Route } from "react-router-dom";
 
-import { library } from '@fortawesome/fontawesome-svg-core';
-import { fab } from '@fortawesome/free-brands-svg-icons';
-import { faHome, faEnvelope } from '@fortawesome/free-solid-svg-icons';
+import { library } from "@fortawesome/fontawesome-svg-core";
+import { fab } from "@fortawesome/free-brands-svg-icons";
+import { faHome, faEnvelope } from "@fortawesome/free-solid-svg-icons";
 
-import './App.scss';
-import './assets/sass/themes.scss';
-import Portfolio from './containers/Portfolio/Portfolio';
-import About from './containers/About/About';
-import Experience from './containers/Experience/Experience';
-import Skills from './containers/Skills/Skills';
-import NotFound from './containers/NotFound/NotFound';
-import Layout from './hoc/Layout/Layout';
-import Theme from './containers/Theme/Theme';
+import "./App.scss";
+import "./assets/sass/themes.scss";
+import Portfolio from "./containers/Portfolio/Portfolio";
+import About from "./containers/About/About";
+import Experience from "./containers/Experience/Experience";
+import Skills from "./containers/Skills/Skills";
+import NotFound from "./containers/NotFound/NotFound";
+import Layout from "./hoc/Layout/Layout";
+import Theme from "./containers/Theme/Theme";
 
 // add Font Awesome icons to library
 library.add(fab, faHome, faEnvelope);
 
-const themeNames = { dark: `dark-theme`, light: `light-theme` }; 
+const themeNames = { dark: `dark-theme`, light: `light-theme` };
 
 const App = (props: any) => {
-
   const [themeName, setThemeName] = useState(themeNames.dark);
 
   return (
@@ -29,9 +28,15 @@ const App = (props: any) => {
       <Theme themeName={themeName}>
         <Layout setThemeName={setThemeName} themeName={themeName}>
           <Switch>
-            <Route path="/portfolio" component={Portfolio} />
+            <Route
+              path="/portfolio"
+              component={() => <Portfolio themeName={themeName} />}
+            />
             <Route path="/experience" component={Experience} />
-            <Route path="/skills" component={() => <Skills themeName={themeName} />} />
+            <Route
+              path="/skills"
+              component={() => <Skills themeName={themeName} />}
+            />
             <Route path="/" exact component={About} />
             {/* <Redirect from="/something" to="/something-else" /> */}
             <Route component={NotFound} />
@@ -40,6 +45,6 @@ const App = (props: any) => {
       </Theme>
     </BrowserRouter>
   );
-}
+};
 
 export default App;
--- src/containers/Portfolio/Portfolio.tsx
+++ src/containers/Portfolio/Portfolio.tsx
@@ -1,11 +1,42 @@
-import React from 'react';
+import React, { Component } from "react";
+import Lottie from "react-lottie";
 
-import './Portfolio.scss'
+import lightSquirrel from "../../assets/lotties/sleeping-squirrel.json";
+import darkSquirrel from "../../assets/lotties/sleeping-squirrel-dark.json";
+import "./Portfolio.scss";
 
-const portfolio = () => {
+class portfolio extends Component<any, any> {
+  render() {
+    const themedSquirrel =
+      this.props.themeName === "dark-theme" ? darkSquirrel : lightSquirrel;
+
+    const defaultOptions = {
+      loop: true,
+      autoplay: true,
+      animationData: themedSquirrel,
+      rendererSettings: {
+        preserveAspectRatio: "xMidYMid slice",
+      },
+    };
+
     return (
-        <div className="Portfolio">Portfolio content coming here soon. For real, check my <a className="Default" target="_blank" rel="noopener noreferrer" href="https://github.com/mkhbragg">GitHub</a>.</div>
+      <div className="Portfolio">
+        <span>
+          Working on it! Probably. In the meantime, check my{" "}
+          <a
+            className="Default"
+            target="_blank"
+            rel="noopener"
+            href="https://github.com/mkhbragg"
+          >
+            GitHub
+          </a>
+          .
+        </span>
+        <Lottie options={defaultOptions} height={400} width={400} />
+      </div>
     );
+  }
 }
 
 export default portfolio;

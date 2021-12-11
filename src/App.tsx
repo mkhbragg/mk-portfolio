c77ab1d59234a64355aba91c9a890147570ac3f6
Mariah Bragg <mk.bragg@pm.me>
2021-12-10 23:27:17 -0500

    Remember theme choice in local storage

--- src/App.tsx
+++ src/App.tsx
@@ -1,27 +1,33 @@
-import React, { useState } from "react";
-import { BrowserRouter, Switch, Route } from "react-router-dom";
+import React, { useEffect, useState } from 'react'
+import { BrowserRouter, Switch, Route } from 'react-router-dom'
 
-import { library } from "@fortawesome/fontawesome-svg-core";
-import { fab } from "@fortawesome/free-brands-svg-icons";
-import { faHome, faEnvelope } from "@fortawesome/free-solid-svg-icons";
+import { library } from '@fortawesome/fontawesome-svg-core'
+import { fab } from '@fortawesome/free-brands-svg-icons'
+import { faHome, faEnvelope } from '@fortawesome/free-solid-svg-icons'
 
-import "./App.scss";
-import "./assets/sass/themes.scss";
-import Portfolio from "./containers/Portfolio/Portfolio";
-import About from "./containers/About/About";
-import Experience from "./containers/Experience/Experience";
-import Skills from "./containers/Skills/Skills";
-import NotFound from "./containers/NotFound/NotFound";
-import Layout from "./hoc/Layout/Layout";
-import Theme from "./containers/Theme/Theme";
+import './App.scss'
+import './assets/sass/themes.scss'
+import Portfolio from './containers/Portfolio/Portfolio'
+import About from './containers/About/About'
+import Experience from './containers/Experience/Experience'
+import Skills from './containers/Skills/Skills'
+import NotFound from './containers/NotFound/NotFound'
+import Layout from './hoc/Layout/Layout'
+import Theme from './containers/Theme/Theme'
 
 // add Font Awesome icons to library
-library.add(fab, faHome, faEnvelope);
+library.add(fab, faHome, faEnvelope)
 
-const themeNames = { dark: `dark-theme`, light: `light-theme` };
+const themeNames = { dark: `dark-theme`, light: `light-theme` }
 
 const App = (props: any) => {
-  const [themeName, setThemeName] = useState(themeNames.dark);
+  const [themeName, setThemeName] = useState(
+    localStorage.getItem('mariah-theme') ?? themeNames.dark
+  )
+
+  useEffect(() => {
+    localStorage.setItem('mariah-theme', themeName)
+  }, [themeName])
 
   return (
     <BrowserRouter>
@@ -44,7 +50,7 @@ const App = (props: any) => {
         </Layout>
       </Theme>
     </BrowserRouter>
-  );
-};
+  )
+}
 
-export default App;
+export default App
